e5edf5b85692170bf833b96b6db7def3848ae1cb
Kristen Norris <krnorris65@gmail.com>
2019-06-20 16:42:43 -0500

    user can search terms in the journal entries

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -7,3 +7,27 @@ document.getElementsByName("mood").forEach(moodInput => {
 })
 
 document.getElementById("dontSave").addEventListener("click", resetValues)
+
+document.getElementById("searchTerm").addEventListener("keypress", () => {
+    if (event.keyCode === 13) {
+        let searchedTerm = document.getElementById("searchTerm").value.toLowerCase()
+        console.log(searchedTerm)
+
+        let searchResults = []
+
+        API.cachedJournalEntries.forEach(entry => {
+
+            for (const value of Object.values(entry)) {
+                if (typeof value === "string") {
+                    if (value.toLowerCase().includes(searchedTerm)) {
+                        if (!searchResults.includes(entry)) {
+                            searchResults.push(entry)
+                        }
+                    }
+                }
+            }
+        })
+        renderJournalEntries(searchResults)
+
+    }
+})
