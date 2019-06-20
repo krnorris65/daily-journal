c9651da323b418f00d399f1c61c3b9a47e98ebb6
Kristen Norris <krnorris65@gmail.com>
2019-06-19 21:25:48 -0500

    fields must be completed before obj is created

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,1 +1,18 @@
 API.getJournalEntries().then(renderJournalEntries)
+
+document.querySelector("#saveEntry").addEventListener("click", () => {
+    let date = document.getElementById("journalDate").value
+    let concept = document.getElementById("journalConcept").value
+    let entry = document.getElementById("journalEntry").value
+    let mood = document.getElementById("journalMood").value
+
+    if(date !== "" && concept !== "" && entry !== ""){
+        let journalObj = {
+            date: date,
+            concept: concept,
+            entry: entry,
+            mood: mood
+        }
+        console.log(journalObj)
+    }
+})
